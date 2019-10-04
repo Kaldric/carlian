6bf518a73d0e5408fb0bbe78d23e8fdcdc9f9d42
Kaldric <55188640+Kaldric@users.noreply.github.com>
2019-10-04 14:55:25 +0300

    person luokka lisätty

---
+++ src/com/company/fi/academy/Person.java
@@ -0,0 +1,35 @@
+package com.company.fi.academy;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.name = name;
+    }
+
+    public int getAge(int age) {
+        return this.name;
+    }
+
+    public String toString() {
+    }
+
+}
